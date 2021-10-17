79756ef56d374345c099992e05e7e388452b66f4
Harsh Gupta <69745424+harshgupta97@users.noreply.github.com>
2021-10-17 09:22:39 +0530

    Array data structure

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,24 @@
 package com.company;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-	    Array numbers = new Array(3);
-		numbers.insert(1);
-	    numbers.insert(2);
-		numbers.insert(3);
-		numbers.insert(4);
-		System.out.println(numbers.indexOf(2));
-	    numbers.print();
+
+		LinkedList<Integer> list = new LinkedList();
+
+//		Map<Integer, String> map = new HashMap<>();
+//		map.put(1, "Mosh");
+//		map.put(2, "Harsh");
+//		map.put(3, "Ajay");
+//		map.remove(3);
+//		var value = map.get(2);
+//		System.out.println(value);
+//		map.containsKey(1); // O(1)
+//		map.containsValue("Mosh"); // O(n)
+//		System.out.println(map);
     }
 }
